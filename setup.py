c344a0928c9e868adb73feec3e12299aba3a3322
Alexey Kotlyarov <alexey@infoxchange.net.au>
2014-06-19 14:59:38 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open('requirements.txt') as requirements, \
         open('test_requirements.txt') as test_requirements:
     setup(
         name='supervisor-logging',
-        version='0.0.1',
+        version='0.0.2',
         description='Stream supervisord logs to a syslog instance',
         author='Infoxchange development team',
         author_email='devs@infoxchange.net.au',
