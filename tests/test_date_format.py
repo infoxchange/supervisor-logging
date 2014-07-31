90c6c54adaf629e9a4e355bcf156b18766b7c2af
Didip Kerabat <didip@newrelic.com>
2014-07-30 17:41:25 -0700

    Added 2 more tests around date formatting.

---
+++ tests/test_date_format.py
@@ -0,0 +1,48 @@
+#
+# Copyright 2014  Infoxchange Australia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Test date_format.
+"""
+
+import os
+import datetime
+from unittest import TestCase
+from supervisor_logging import PalletFormatter
+
+
+class SupervisorLoggingDateFormatTestCase(TestCase):
+    """
+    Tests date format.
+    """
+
+    def test_default_date_format(self):
+        """
+        Test default date format.
+        """
+        d = datetime.datetime(2000, 1, 1, 1, 0, 0)
+        date_format = PalletFormatter().date_format()
+        self.assertEqual(d.strftime(date_format), '2000-01-01T01:00:00')
+
+    def test_custom_date_format(self):
+        """
+        Test custom date format.
+        """
+        d = datetime.datetime(2000, 1, 1, 1, 0, 0)
+        os.environ['SYSLOG_DATE_FORMAT'] = '%b %d %H:%M:%S'
+        date_format = PalletFormatter().date_format()
+        self.assertEqual(d.strftime(date_format), 'Jan 01 01:00:00')
+        os.environ['SYSLOG_DATE_FORMAT'] = PalletFormatter.DFLT_DATE_FORMAT
+
